a696aa865237208a7c43f115294e7901c2fa8b39
k-awn <iamgoingtobealivetoday@gmail.com>
2024-11-19 18:37:48 +0200

    Last version before product draft

---
+++ experiments/dictappend.py
@@ -0,0 +1,5 @@
+dictionary = {1:2, "two":2, "three":3}
+def append():
+    dictionary["four"] = 4
+    print(dictionary[1])
+append()
--- experiments/variablevariablenames.py
+++ experiments/variablevariablenames.py
@@ -3,3 +3,4 @@ for i in range(5):
   variables[i] = i
 print(variables[0])
 print(variables[1])
+print(variables)
