a0cdc96a5f3d35d750a32cd5ca6ad1fe01148103
k-awn <iamgoingtobealivetoday@gmail.com>
2025-09-21 17:50:10 +0300

    removed app's old versions

--- Product-Draft/buttonpress.py
+++
@@ -1,7 +0,0 @@
-from pyautogui import press, hold
-from time import sleep as wait
-
-wait(2)
-with hold('shift'):
-    press('f')
-
--- Product-Draft/thangamajang.py
+++
@@ -1,5 +0,0 @@
-import pyautogui
-
-while pyautogui.hold("ctrl"): 
-    pyautogui.press("t") 
-    break
